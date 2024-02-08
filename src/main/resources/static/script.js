c39cfe89b789c025f0a76d59b16a65d40df2bd25
Gabebabe009 <gabal8187@oslomet.no>
2024-02-08 14:15:20 +0100

    Finished off the code by finishing the array and the printOut and deleteTickets function

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,13 +1,14 @@
+
 let tickets = [];  //Her endres  arrayet fra buyTicket til å være globalt tilgjengelig slik at vi kan bruke det senere i deleteTickets
 
 function buyTicket() {
 
-    const chosenMovie = document.getElementById("Movies").value;
-    const wrongNumber = document.getElementById("numberTickets").value;
-    const chosenFirstName = document.getElementById("chooseFirstName").value;
-    const chosenLastName = document.getElementById("chooseLastName").value;
-    const chosenPhoneNr = document.getElementById("choosePhoneNr").value;
-    const chosenEmail = document.getElementById("chooseMail").value;
+    let chosenMovie = document.getElementById("Movies").value;
+    let wrongNumber = document.getElementById("numberTickets").value;
+    let chosenFirstName = document.getElementById("chooseFirstName").value;
+    let chosenLastName = document.getElementById("chooseLastName").value;
+    let chosenPhoneNr = document.getElementById("choosePhoneNr").value;
+    let chosenEmail = document.getElementById("chooseMail").value;
 
     const ticket1 = {  //Oppretter et objekt og legger inn de opprettede variablene.
         movie: chosenMovie,
@@ -17,6 +18,9 @@ function buyTicket() {
         phoneNr: chosenPhoneNr,
         mail: chosenEmail
     };
+    tickets.push(ticket1);
+    console.log(tickets);
+    printOut();
 
     function verification(value, errorMessageId, errorMessage) {
         if (value === "") {
@@ -24,15 +28,16 @@ function buyTicket() {
         } else {
             document.getElementById(errorMessageId).innerHTML = "";
         }
+        console.log("2");
+
     }
 
-    verification(ticket1.movie,"wrongMovie","Choose a movie")
     verification(ticket1.number, "wrongNumber", "You have to write in an amount from 1-->");
     verification(ticket1.firstName, "wrongFristName", "Write in a first name");
     verification(ticket1.lastName, "wrongLastName", "Write in a first last name");
     verification(ticket1.phoneNr, "wrongPhoneNr", "Write in a phone number");
     verification(ticket1.mail, "wrongMail", "Write in an email");
-
+}
     function printOut() { //kjører gjennom en for-løkke for å skrive ut verdier vi har laget i objektet og satt inn i arrayet.
         let ut = "";
         for (let i = 0; i < tickets.length; i++) {
@@ -41,11 +46,12 @@ function buyTicket() {
                 " " + tickets[i].mail;
         }
         document.getElementById("utskrift").innerHTML = ut;
+        console.log(ut);
+        console.log("3");
     }
 
-    tickets = [];
-    function deleteTickets() {
+    function deleteTickets(){
+        tickets = [];
         console.log(tickets);
         printOut();
     }
-}
